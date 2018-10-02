921774a91963bd351123f9790fbb4a5194f9d376
Michael Westfall <mwestfall@gemini.edu>
2018-10-02 15:50:12 +0000

    added vxWorks support to osdClockFuncs.c
    git-svn-id: http://sbfsvn02/gemini-sw/gem/trunk/support/bancomm@3617 303ae0b8-5cfe-405b-ba84-a597b98f2cc0

--- bancommApp/src/osdClockFuncs.c
+++ bancommApp/src/osdClockFuncs.c
@@ -2,16 +2,29 @@
 #include "osdClockFuncs.h"
 
 #if defined (__rtems__)
+#include <rtems.h>
+#include <bsp.h>
+
 extern rtems_configuration_table Configuration;
 extern rtems_interval rtemsTicksPerSecond;
 extern double rtemsTicksPerSecond_double;
 extern double rtemsTicksPerTwoSeconds_double;
 #endif
 
+#if defined (vxWorks)
+#include <sysLib.h>
+#include <tickLib.h>
+#endif
+
+
 void sysClockOn(void)
 {
 #if defined (__rtems__)
    BSP_connect_clock_handler();
+#endif
+
+#if defined (vxWorks)
+   sysClkEnable();
 #endif
 }
 
@@ -19,6 +32,10 @@ void sysClockOff(void)
 {
 #if defined  (__rtems__)
    BSP_disconnect_clock_handler();
+#endif
+
+#if defined (vxWorks)
+   sysClkDisable();
 #endif
 }
 
@@ -28,6 +45,10 @@ void clock_tick(void)
 #if defined (__rtems__)
    rtems_clock_tick();
 #endif
+
+#if defined (vxWorks)
+   tickAnnounce();
+#endif
 }
 
 int clock_rate_get(void) 
@@ -36,9 +57,11 @@ int clock_rate_get(void)
    rtems_interval ticksPerSecond;
    rtems_clock_get(RTEMS_CLOCK_GET_TICKS_PER_SECOND, &ticksPerSecond);
    return (int)ticksPerSecond;
-#else
-return -1;
 #endif
+#if defined (vxWorks)
+   return sysClkRateGet();
+#endif
+return -1;
 }
 
 int clock_rate_set(int  rate)
@@ -49,6 +72,10 @@ int clock_rate_set(int  rate)
    rtems_clock_get (RTEMS_CLOCK_GET_TICKS_PER_SECOND, &rtemsTicksPerSecond);
    rtemsTicksPerSecond_double = rtemsTicksPerSecond;
    rtemsTicksPerTwoSeconds_double = rtemsTicksPerSecond_double * 2.0;
+#endif
+
+#if defined (vxWorks)
+   sysClkRateSet(rate);
 #endif
 
    return 0;
